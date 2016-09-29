3acaf2d0518c784849cec1c2244e6c54821f6176
Matthew Maurer <matthew.r.maurer@gmail.com>
2016-09-29 17:12:28 -0400

    Make LLVM Plugin work as a library
    If the library is loaded, but the configuration isn't ready in time, assume
    AT&T syntax and initialize.

--- plugins/llvm/llvm_main.ml
+++ plugins/llvm/llvm_main.ml
@@ -4,6 +4,18 @@ include Self()
 
 external init : unit -> int = "disasm_llvm_init_stub" "noalloc"
 
+let loaded = ref false
+
+let library_load (syntax : string) : unit =
+  if not !loaded then (
+    Unix.putenv "BAP_LLVM_OPTIONS" ("-x86-asm-syntax=" ^ syntax);
+    let r = init () in
+    if r < 0 then
+      eprintf "LLVM initialization failed with error %d\n" r
+    else
+      loaded := true
+  )
+
 let () =
   let () = Config.manpage [
       `S "DESCRIPTION";
@@ -16,8 +28,6 @@ let () =
     let doc = sprintf "Choose style of code for x86 syntax between %s"
       @@ Config.doc_enum names in
     Config.(param (enum names) ~default:"att" "x86-syntax" ~doc) in
-  Config.when_ready (fun {Config.get=(!)} ->
-      Unix.putenv "BAP_LLVM_OPTIONS" ("-x86-asm-syntax=" ^ !x86_syntax);
-      let r = init () in
-      if r < 0 then
-        eprintf "LLVM initialization failed with error %d\n" r)
+  Config.when_ready (fun {Config.get=(!)} -> library_load !x86_syntax)
+
+let () = library_load "att"
